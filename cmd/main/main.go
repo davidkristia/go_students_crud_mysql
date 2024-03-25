872709d5bd057c0fb08f406861149ab458e6a59b
davidkristia <128303290+davidkristia@users.noreply.github.com>
2024-03-25 16:03:19 +0700

    Add files via upload

---
+++ cmd/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"https://github.com/davidkristia/go_students_crud_mysql/pkg/routes"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterStudentsRoutes(r)
+	http.Handle("/", r)
+	fmt.Println("Starting Server localhost:8080")
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
